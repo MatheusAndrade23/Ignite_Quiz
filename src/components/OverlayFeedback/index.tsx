6be53719887ef848550b7ba44ecbb58848012fee
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2023-04-23 20:47:58 -0300

    Overlay Feedback Animation

--- src/components/OverlayFeedback/index.tsx
+++ src/components/OverlayFeedback/index.tsx
@@ -1,5 +1,14 @@
+import { useEffect } from "react";
+
 import { useWindowDimensions } from "react-native";
-import Animated from "react-native-reanimated";
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withSequence,
+  withTiming,
+} from "react-native-reanimated";
+
 import { Canvas, Rect, BlurMask } from "@shopify/react-native-skia";
 import { THEME } from "../../styles/theme";
 
@@ -14,12 +23,29 @@ type Props = {
 };
 
 export function OverlayFeedback({ status }: Props) {
+  const opacity = useSharedValue(0);
+
   const { height, width } = useWindowDimensions();
+
+  const styleAnimated = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+    };
+  });
 
   const color = STATUS[status];
 
+  useEffect(() => {
+    opacity.value = withSequence(
+      withTiming(1, { duration: 400, easing: Easing.bounce }),
+      withTiming(0)
+    );
+  }, [status]);
+
   return (
-    <Animated.View style={{ width, height, position: "absolute" }}>
+    <Animated.View
+      style={[{ width, height, position: "absolute" }, styleAnimated]}
+    >
       <Canvas style={{ flex: 1 }}>
         <Rect x={0} y={0} width={width} height={height} color={color}>
           <BlurMask blur={50} style="inner" />
